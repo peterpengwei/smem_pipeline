304b5f06fc60cea750c9f53283476b67c33d07ba
Licheng-Guo <lcguo666@gmail.com>
2017-12-19 21:44:09 +0800

    [in process] use ip core

--- RAM_curr.v
+++ RAM_curr.v
@@ -60,27 +60,9 @@ module RAM_curr_mem(
 	// reg [112:0] curr_queue [`MAX_READ - 1:0][100:0];
 	// reg [112:0] mem_queue  [`MAX_READ - 1:0][100:0];
 	// reg [112:0] curr_queue [`MAX_READ*`READ_LEN - 1:0];
-	reg [112:0] mem_queue  [`MAX_READ*`READ_LEN - 1:0];
+	// reg [112:0] mem_queue  [`MAX_READ*`READ_LEN - 1:0];
 	reg [6:0] mem_size_queue[`MAX_READ - 1:0]; //mem_size = 7bits;
 	reg [6:0] ret_queue[`MAX_READ - 1:0] ; //ret = 7 bits;
-	
-	/* //curr queue
-	always@(posedge clk) begin
-		
-		//port A
-		if(curr_we_1) begin
-			curr_queue[curr_read_num_1 * `READ_LEN + curr_addr_1] <= {curr_data_1[230:224],curr_data_1[198:192],curr_data_1[160:128],curr_data_1[96:64],curr_data_1[32:0]};
-			//curr_q_1 <= curr_data_1;
-		end
-		
-		//[very important] use stall signal as the read_en. 
-		if(!stall) begin
-			{curr_q_2[230:224],curr_q_2[198:192],curr_q_2[160:128],curr_q_2[96: 64],curr_q_2[32: 0]} <= curr_queue[curr_read_num_2 * `READ_LEN + curr_addr_2];
-			{curr_q_2[255:231],curr_q_2[223:199],curr_q_2[191:161],curr_q_2[127:97],curr_q_2[63:33]} <= 0;
-		end
-	end
-	
-	 */
 	
 	RAM_Curr_Queue curr_queue(
 		.clk(clk),
@@ -96,14 +78,26 @@ module RAM_curr_mem(
 	assign {curr_q_2[255:231],curr_q_2[223:199],curr_q_2[191:161],curr_q_2[127:97],curr_q_2[63:33]} = 0;
 	
 	
-	//mem queue
+/* 	//mem queue
 	always@(posedge clk) begin
 		//port A
 		if(mem_we_1) begin
 			mem_queue[mem_read_num_1 * `READ_LEN + mem_addr_1] <= {mem_data_1[230:224],mem_data_1[198:192],mem_data_1[160:128],mem_data_1[96:64],mem_data_1[32:0]};
 		end
 
-	end
+	end */
+	reg [`READ_NUM_WIDTH+1 - 1:0] output_result_ptr;
+	reg [6:0] already_output_num; //mem number, not read number
+	wire [`CURR_QUEUE_ADDR_WIDTH-1 : 0] mem_addr = mem_we_1 ? (mem_read_num_1 * `READ_LEN + mem_addr_1) : (output_result_ptr * `READ_LEN + already_output_num);
+	wire [112:0] mem_q_out;
+	RAM_Mem_Queue mem_queue(
+		.clk(clk),
+		
+		.mem_we_1(mem_we_1),
+		.addr(mem_addr),
+		.data({mem_data_1[230:224],mem_data_1[198:192],mem_data_1[160:128],mem_data_1[96:64],mem_data_1[32:0]}),
+		.q(mem_q_out)
+	);
 	
 	//params
 	reg [`READ_NUM_WIDTH+1 - 1:0] done_counter;
@@ -148,12 +142,30 @@ module RAM_curr_mem(
 		end
 	end
 	
-	reg [`READ_NUM_WIDTH+1 - 1:0] output_result_ptr;
+	
 	reg [6:0] output_mem_ptr;
 	reg [6:0] curr_size;//mem size, not read size
-	reg [6:0] already_output_num; //mem number, not read number
+	
 	reg group_start; //indicate the initial of a read's data
 	reg odd_even_flag;
+	reg group_start_q;
+	
+	always@(posedge clk) group_start_q <= group_start;
+	
+	always@(*) begin
+		if(group_start_q) begin
+			output_data[9:0]     = output_result_ptr; //read num
+			output_data[63:10]   = 0;
+			output_data[70:64]   = mem_size_queue[output_result_ptr];
+			output_data[127:71]  = 0;
+			output_data[159:128] = {25'b0, ret_queue[output_result_ptr]};
+			output_data[511:160] = 0;
+		end
+		else begin
+			{output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} = mem_q_out;
+			{output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} = 0;
+		end
+	end
 	
 	always@(posedge clk) begin
 		if(!reset_n) begin
@@ -161,7 +173,7 @@ module RAM_curr_mem(
 			output_mem_ptr <= 0;
 			group_start <= 1;
 			output_valid <= 0;
-			output_data <= 0;
+			// output_data <= 0;
 			output_finish <= 0;
 			already_output_num <= 0;
 			curr_size <= 0;
@@ -172,73 +184,54 @@ module RAM_curr_mem(
 				if(output_result_ptr < batch_size) begin 
 					if(group_start) begin
 						output_valid		 <= 1;
-						output_data[9:0]     <= output_result_ptr; //read num
-						output_data[63:10]   <= 0;
-						output_data[70:64]   <= mem_size_queue[output_result_ptr];
-						output_data[127:71]  <= 0;
-						output_data[159:128] <= {25'b0, ret_queue[output_result_ptr]};
-						output_data[511:160] <= 0;
+						// output_data[9:0]     <= output_result_ptr; //read num
+						// output_data[63:10]   <= 0;
+						// output_data[70:64]   <= mem_size_queue[output_result_ptr];
+						// output_data[127:71]  <= 0;
+						// output_data[159:128] <= {25'b0, ret_queue[output_result_ptr]};
+						// output_data[511:160] <= 0;
 						group_start <= 0;
 						curr_size <= mem_size_queue[output_result_ptr];
 						already_output_num <= 0;
 						odd_even_flag <= 0;
 					end
-					else if(already_output_num < curr_size - 1) begin
-						if(odd_even_flag == 0) begin
+					else if(already_output_num < curr_size) begin
+/* 						if(odd_even_flag == 0) begin
 							output_valid <= 0;
 							
-							{output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_queue[output_result_ptr * `READ_LEN + already_output_num];
-							{output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
+							// {output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_data_q;
+							// {output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
 						
 							odd_even_flag <= 1;
 						end
 						else if (odd_even_flag == 1) begin
 							output_valid <= 1;
 							
-							{output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_queue[output_result_ptr * `READ_LEN + already_output_num];
-							{output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
+							// {output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_data_q;
+							// {output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
 
 							odd_even_flag <= 0;
-						end
-						
+						end */
+						output_valid <= 1;
 						already_output_num <= already_output_num + 1;
 					end
-					else if(already_output_num == curr_size - 1) begin				
+					// else if(already_output_num == curr_size - 1) begin				
 						
-						if(odd_even_flag == 0) begin							
-							{output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_queue[output_result_ptr * `READ_LEN + already_output_num];
-							{output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
+						// if(odd_even_flag == 0) begin							
+							// {output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_data_q;
+							// {output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
 							
-							{output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= 0;
-							{output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
-						end
-						else if (odd_even_flag == 1) begin						
-							{output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_queue[output_result_ptr * `READ_LEN + already_output_num];
-							{output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
-						end
+							// {output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= 0;
+							// {output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
+						// end
+						// else if (odd_even_flag == 1) begin						
+							// {output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_data_q;
+							// {output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
+						// end
 						
-						output_valid <= 1;
-						odd_even_flag <= 0;
-						already_output_num <= already_output_num + 1;						
-					end
-					// else if(already_output_num < curr_size - 1) begin
 						// output_valid <= 1;
-						
-						// {output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_queue[output_result_ptr * `READ_LEN + already_output_num];
-						// {output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
-						
-						// {output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_queue[output_result_ptr * `READ_LEN + already_output_num + 1];
-						// {output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
-						// already_output_num <= already_output_num + 2;	
-					// end
-					// else if(already_output_num == curr_size - 1) begin
-						// output_valid <= 1;
-						
-						// {output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_queue[output_result_ptr * `READ_LEN + already_output_num];
-						// {output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
-						// output_data[511:256] <= 0;
-						
-						// already_output_num <= already_output_num + 1;
+						// odd_even_flag <= 0;
+						// already_output_num <= already_output_num + 1;						
 					// end
 					else if(already_output_num == curr_size) begin
 						output_valid <= 0; //[important] during the output process there will be a gap between each mem group!
@@ -288,15 +281,23 @@ module RAM_Curr_Queue(
 	end
 endmodule
 
-// module RAM_Mem_Queue(
-	// input clk,
+module RAM_Mem_Queue(
+	input clk,
 	
-	// input we_1,
-	// input [`CURR_QUEUE_ADDR_WIDTH-1 : 0] addr_1,
-	// input [112:0] data,
-	// output reg [112:0] q_1,
+	input mem_we_1,
+	input [`CURR_QUEUE_ADDR_WIDTH-1 : 0] addr,
+	input [112:0] data,
+	output reg [112:0] q
+);
+	reg [112:0] mem_queue  [`MAX_READ*`READ_LEN - 1:0];
 	
-	// input [`CURR_QUEUE_ADDR_WIDTH-1 : 0] addr_2, 
-	// output reg [112:0] q_2,
+	always@(posedge clk) begin
+		if(mem_we_1) begin
+			mem_queue[addr] <= data;
+		end
+		else begin
+			q <= mem_queue[addr];
+		end
+	end
 
-// );
+endmodule
