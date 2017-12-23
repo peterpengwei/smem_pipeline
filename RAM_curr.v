9a3e79404d1f47fee82058a070a0dd6cb78ece7c
Licheng-Guo <lcguo666@gmail.com>
2017-12-23 18:42:06 +0800

    fix stall error

--- RAM_curr.v
+++ RAM_curr.v
@@ -98,7 +98,7 @@ module RAM_curr_mem(
 	wire [`MEM_QUEUE_ADDR_WIDTH-1 : 0] mem_addr_A = (mem_read_num_1 * `READ_MAX_MEM + mem_addr_1);
 	wire [112:0] mem_data_A = {mem_data_1[230:224],mem_data_1[198:192],mem_data_1[160:128],mem_data_1[96:64],mem_data_1[32:0]};
 	reg [`READ_NUM_WIDTH+1 - 1:0] output_result_ptr;
-	reg [6:0] already_output_num, already_output_num_q; //mem number, not read number
+	reg [6:0] already_output_num, already_output_num_q, already_output_num_qq; //mem number, not read number
 	
 	//add one pipeline stage for mem write
 	reg mem_we_1_q;
@@ -114,7 +114,13 @@ module RAM_curr_mem(
 	wire [`MEM_QUEUE_ADDR_WIDTH-1 : 0] mem_addr_A_out = (output_result_ptr * `READ_MAX_MEM + already_output_num);
 	wire [`MEM_QUEUE_ADDR_WIDTH-1 : 0] mem_addr_B_out = (output_result_ptr * `READ_MAX_MEM + already_output_num + 1);
 	
-	wire [`MEM_QUEUE_ADDR_WIDTH-1 : 0] mem_addr_A_q_MUX = mem_we_1_q ? mem_addr_A_q : mem_addr_A_out;
+	reg [`MEM_QUEUE_ADDR_WIDTH-1 : 0] mem_addr_A_out_q, mem_addr_B_out_q;
+	always@(posedge clk) begin
+		mem_addr_A_out_q <= mem_addr_A_out;
+		mem_addr_B_out_q <= mem_addr_B_out;
+	end
+	
+	wire [`MEM_QUEUE_ADDR_WIDTH-1 : 0] mem_addr_A_q_MUX = mem_we_1_q ? mem_addr_A_q : mem_addr_A_out_q;
 
 	
 	
@@ -124,6 +130,7 @@ module RAM_curr_mem(
 	
 	RAM_Mem_Queue mem_queue(
 		.clk(clk),
+		.read_en(!stall),
 		
 		.mem_we_1(mem_we_1_q),
 		.addr_1(mem_addr_A_q_MUX),
@@ -131,7 +138,7 @@ module RAM_curr_mem(
 		.q_1(mem_q_out_A),
 		
 		.mem_we_2(1'b0),
-		.addr_2(mem_addr_B_out),
+		.addr_2(mem_addr_B_out_q),
 		.data_2(113'b0),
 		.q_2(mem_q_out_B)
 	);
@@ -184,14 +191,20 @@ module RAM_curr_mem(
 	reg [6:0] curr_size;//mem size, not read size
 	
 	reg group_start; //indicate the initial of a read's data
-	reg odd_even_flag;
-	reg group_start_q;
+	reg group_start_q, group_start_qq;
 	
-	always@(posedge clk) group_start_q <= group_start;
-	always@(posedge clk) already_output_num_q <= already_output_num;
+	always@(posedge clk) begin
+		if(!stall) begin
+			group_start_q <= group_start;
+			group_start_qq <= group_start_q;
+			
+			already_output_num_q <= already_output_num;
+			already_output_num_qq <= already_output_num_q;
+		end
+	end
 	
 	always@(*) begin
-		if(group_start_q) begin
+		if(group_start_qq) begin
 			output_data[9:0]     = output_result_ptr; //read num
 			output_data[63:10]   = 0;
 			output_data[70:64]   = mem_size_queue[output_result_ptr];
@@ -199,14 +212,14 @@ module RAM_curr_mem(
 			output_data[159:128] = {25'b0, ret_queue[output_result_ptr]};
 			output_data[511:160] = 0;
 		end
-		else if(already_output_num_q < curr_size - 1) begin
+		else if(already_output_num_qq < curr_size - 1) begin
 			{output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} = mem_q_out_A;
 			{output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} = 0;
 			
 			{output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_q_out_B;
 			{output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
 		end
-		else if(already_output_num_q == curr_size - 1) begin
+		else if(already_output_num_qq == curr_size - 1) begin
 			{output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} = mem_q_out_A;
 			{output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} = 0;
 			
@@ -219,87 +232,65 @@ module RAM_curr_mem(
 		end
 	end
 	
+	reg output_valid_d, output_finish_d;
+	//add one pipeline stage for output_valid and output_finish;
+	always@(posedge clk) begin
+		if(!stall) begin
+			output_valid <= output_valid_d;
+			output_finish <= output_finish_d;
+		end
+		else begin
+			output_valid <= 0;
+		end
+	end
+	
 	always@(posedge clk) begin
 		if(!reset_n) begin
 			output_result_ptr <= 0;
 			output_mem_ptr <= 0;
 			group_start <= 1;
-			output_valid <= 0;
+			output_valid_d <= 0;
 			// output_data <= 0;
-			output_finish <= 0;
+			output_finish_d <= 0;
 			already_output_num <= 0;
 			curr_size <= 0;
-			odd_even_flag <= 0;
+
 		end
 		else if(output_permit) begin
 			if(!stall) begin
 				if(output_result_ptr < batch_size) begin 
 					if(group_start) begin
-						output_valid		 <= 1;
-						// output_data[9:0]     <= output_result_ptr; //read num
-						// output_data[63:10]   <= 0;
-						// output_data[70:64]   <= mem_size_queue[output_result_ptr];
-						// output_data[127:71]  <= 0;
-						// output_data[159:128] <= {25'b0, ret_queue[output_result_ptr]};
-						// output_data[511:160] <= 0;
+						output_valid_d		 <= 1;
+
 						group_start <= 0;
 						curr_size <= mem_size_queue[output_result_ptr];
 						already_output_num <= 0;
-						odd_even_flag <= 0;
 					end
 					else if(already_output_num < curr_size - 1) begin
- 						// if(odd_even_flag == 0) begin
-							// output_valid <= 0;
-							
-							// {output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_data_q;
-							// {output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
-						
-							// odd_even_flag <= 1;
-						// end
-						// else if (odd_even_flag == 1) begin
-							// output_valid <= 1;
-							
-							// {output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_data_q;
-							// {output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
 
-							// odd_even_flag <= 0;
-						// end 
-						output_valid <= 1;
 						already_output_num <= already_output_num + 2;
+					
 					end
 					else if(already_output_num == curr_size - 1) begin				
 						
-						// if(odd_even_flag == 0) begin							
-							// {output_data[230:224],output_data[198:192],output_data[160:128],output_data[96:64],output_data[32:0]} <= mem_data_q;
-							// {output_data[255:231],output_data[223:199],output_data[191:161],output_data[127:97],output_data[63:33]} <= 0;
-							
-							// {output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= 0;
-							// {output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
-						// end
-						// else if (odd_even_flag == 1) begin						
-							// {output_data[486:480],output_data[454:448],output_data[416:384],output_data[352:320],output_data[288:256]} <= mem_data_q;
-							// {output_data[511:487],output_data[479:455],output_data[447:417],output_data[383:353],output_data[319:289]} <= 0;
-						// end
-						
-						output_valid <= 1;
-						// odd_even_flag <= 0;
 						already_output_num <= already_output_num + 1;						
+					
 					end
 					else if(already_output_num == curr_size) begin
-						output_valid <= 0; //[important] during the output process there will be a gap between each mem group!
+						output_valid_d <= 0; //[important] during the output process there will be a gap between each mem group!
 						output_result_ptr <= output_result_ptr + 1;
 						group_start <= 1;
 					end
 				end
 				else begin
-					output_valid <= 0;
-					output_finish <= 1;
+					output_valid_d <= 0;
+					output_finish_d <= 1;
 				
 				end
 			end
-			else begin
-				output_valid <= 0;
-			end
+			// else begin
+				// output_valid_d <= 0;
+			// end
 		end
 	end
 	
@@ -335,7 +326,7 @@ endmodule
 
 module RAM_Mem_Queue(
 	input clk,
-	
+	input read_en,
 	input mem_we_1,
 	input [`MEM_QUEUE_ADDR_WIDTH-1 : 0] addr_1,
 	input [112:0] data_1,
@@ -352,14 +343,14 @@ module RAM_Mem_Queue(
 		if(mem_we_1) begin
 			mem_queue[addr_1] <= data_1;
 		end
-		else begin
+		if(read_en) begin
 			q_1 <= mem_queue[addr_1];
 		end
 		
 		if(mem_we_2) begin
 			mem_queue[addr_2] <= data_2;
 		end
-		else begin
+		if(read_en) begin
 			q_2 <= mem_queue[addr_2];
 		end
 	end
